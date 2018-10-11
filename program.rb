f8555f3f107513480e3cf359d21189c381678c14
“Tyler <tylerpalef@gmail.com>
2018-10-10 21:07:16 -0400

    created a ruby test file to iterate through an array. so far have it pritning out an a array but only numbers greater than 5

---
+++ program.rb
@@ -0,0 +1,8 @@
+arr = [1,2,3,4,5,6,7,8,9,10]
+
+arr.each do |num|
+  if num > 5
+    print num
+  else
+end 
+end
---
+++ program_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'program.rb'
+
+class TestProgram < MiniTest::Test
+   # tests go in here
+
+
+  def test_argument_takes_string
+  arg(word)
+
+  end
+
+end
