8855858608ec588f168903766a5ba45ea04efdce
“Tyler <tylerpalef@gmail.com>
2018-10-10 21:13:37 -0400

    successfully used unshift to add a 0 to the beginning of the array. commented out other lines to show it

--- program.rb
+++ program.rb
@@ -1,9 +1,10 @@
 arr = [1,2,3,4,5,6,7,8,9,10]
 arr << 11
+arr.unshift(0)
 
 arr.each do |num|
-  if num > 5 && num % 2 == 1
+  # if num > 5 && num % 2 == 1
     print num
-  else
-end
+  # else
+# end
 end
